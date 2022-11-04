26d1c767d0065c1079296ffae8ce269358a7c437
Czarek <czarekkuzma2@gmail.com>
2022-11-04 10:13:06 +0100

    changed tests

--- calorieTracker/tests/api/test_calorie_goal.py
+++ calorieTracker/tests/api/test_calorie_goal.py
@@ -15,3 +15,9 @@ def test_add_calorie_goal():
     response = client.post("/api/calorieGoal/", data)
     
     assert response.status_code == 400
+    
+@pytest.mark.django_db
+def test_get_calorie_goal():
+    response = client.get("/api/calorieGoal")
+    
+    assert response.status_code == 200
